3905b601dd6b1fb3245b1b0874f90126c017fec3
Ariel Tan <ariel@Ariels-MacBook-Pro.local>
2019-09-11 11:38:56 +0800

    update the main js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
-
-
+import VueSimpleMarkdown from 'vue-simple-markdown'
+// You need a specific loader for CSS files like https://github.com/webpack/css-loader
+import 'vue-simple-markdown/dist/vue-simple-markdown.css'
+ 
+Vue.use(VueSimpleMarkdown)
 Vue.config.productionTip = false
 
 
 new Vue({
   render: h => h(App),
-  store,
   router
 }).$mount('#app')
