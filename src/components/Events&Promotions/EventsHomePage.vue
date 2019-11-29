3547a667c52ce351dc0b720991cd92c03971b968
Ariel Tan <arieltan@ambercreative.sg>
2019-11-29 12:58:06 +0800

    remove the slider for events and promotions

--- src/components/Events&Promotions/EventsHomePage.vue
+++ src/components/Events&Promotions/EventsHomePage.vue
@@ -23,30 +23,6 @@
                         </li>
                     </ul>
                 </nav>
-                <div class="tab-content popular-searches py-3 px-3 px-sm-0" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-events" role="tabpanel" aria-labelledby="nav-events-tab">
-                        <vueper-slides class="no-shadow" :visible-slides="3" :slide-multiple="true" :slide-ratio="1/4" :dragging-distance="400" :bullets="false" :arrows-outside="true" :fixedHeight="true" :breakpoints="{ 800: { visibleSlides: 2 } }">
-                            <div slot="arrowLeft" color="white" large><img src="https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group29.svg" alt="left-arrow" width="25px"></div>
-                            <div slot="arrowRight" color="white" large><img src="https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group28.svg" alt="right-arrow" width="25px;"></div>
-                            <vueper-slide class="mr-3" style="width:300px;" v-for="event in events" :key="event.id" :image="event.cover_image" >
-                                <div slot="slideContent">
-                                    <div class="slider-white-box">{{ event.title }}</div>
-                                </div>
-                            </vueper-slide>
-                        </vueper-slides>
-                    </div>
-                    <div class="tab-pane fade" id="nav-promotions" role="tabpanel" aria-labelledby="nav-promotions-tab">
-                        <vueper-slides class="no-shadow" :visible-slides="3" slide-multiple :slide-ratio="1/4" :dragging-distance="70" :bullets="false" :arrows-outside="true" :fixedHeight="true" :breakpoints="{ 800: { visibleSlides: 2 } }">
-                            <div slot="arrowLeft" color="white" large><img src="https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group29.svg" alt="left-arrow" width="25px"></div>
-                            <div slot="arrowRight" color="white" large><img src="https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group28.svg" alt="right-arrow" width="25px;"></div>
-                            <vueper-slide class="mr-3" style="width:300px;" v-for="promotion in promotions" :key="promotion.id" :image="promotion.cover_image" >
-                                <div slot="slideContent">
-                                    <div class="slider-white-box">{{ promotion.title }}</div>
-                                </div>
-                            </vueper-slide>
-                        </vueper-slides>
-                    </div>
-                </div>
                 <div class="text-center mt-5">
                     <div class="link">
                         <router-link :to="{ name:'events&promotions' }" class="link-text">See All Events & Promotions</router-link>
