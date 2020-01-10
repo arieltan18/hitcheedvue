67b2fecfca212be0d2dfa32d68d5958d93481100
Ariel Tan <arieltan@ambercreative.sg>
2020-01-10 17:32:15 +0800

    update explore icons

--- src/components/Explore/Explore.vue
+++ src/components/Explore/Explore.vue
@@ -9,12 +9,24 @@
             <div class="categories-section row mb-4" v-if="!isMobile()">
                 <div class="d-flex justify-content-center align-self-center align-items-center mb-3" style="width:100%;">
                     <router-link id="hotels" class="row1 mr-2 header-link" :to="{ name: 'professionalsByCategory', params: { category: 'hotels' }}">
+                        <div class="icon-block">
+                            <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                            <div class="icon-text mt-2">Hotels</div>
+                        </div>
                     </router-link>
                     <router-link id="bridal-studios" class="row1 mr-2 header-link" :to="{ name: 'professionalsByCategory', params: { category: 'bridal-studios' }}">
+                        <div class="icon-block">
+                            <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                            <div class="icon-text mt-2">Bridal Studios</div>
+                        </div>
                     </router-link>
                 </div>
                 <div class="d-flex justify-content-center" style="width:100%;">
                     <router-link id="venues" class="row2 mr-2 header-link" :to="{ name: 'professionalsByCategory', params: { category: 'venues' }}">
+                        <div class="icon-block">
+                            <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                            <div class="icon-text mt-2">Venues</div>
+                        </div>
                     </router-link>
                     <router-link id="photographers" class="row2 mr-2 header-link" :to="{ name: 'professionalsByCategory', params: { category: 'photographers' }}">
                     </router-link>
@@ -24,10 +36,22 @@
             </div>
             <div class="categories-section row mb-4" v-else-if="isMobile()">
                 <router-link id="hotels" class="image header-link" :to="{ name: 'professionalsByCategory', params: { category: 'hotels' }}">
+                    <div class="icon-block">
+                        <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                        <div class="icon-text mt-2">Hotels</div>
+                    </div>
                 </router-link>
                 <router-link id="bridal-studios" class="image header-link" :to="{ name: 'professionalsByCategory', params: { category: 'bridal-studios' }}">
+                    <div class="icon-block">
+                        <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                        <div class="icon-text mt-2">Bridal Studios</div>
+                    </div>
                 </router-link>
                 <router-link id="venues" class="image header-link" :to="{ name: 'professionalsByCategory', params: { category: 'venues' }}">
+                    <div class="icon-block">
+                        <img class="icon" src="https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/hotel-icon.png" alt="">
+                        <div class="icon-text mt-2">Venues</div>
+                    </div>
                 </router-link>
                 <router-link id="photographers" class="image header-link" :to="{ name: 'professionalsByCategory', params: { category: 'photographers' }}">
                 </router-link>
@@ -86,27 +110,27 @@ h1
 }
 #hotels
 {
-    background-image: url('https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group1094%402x.png');
+    background-image: url('https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/banquet-beautiful-catering.jpg');
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    cursor: pointer;
+    text-decoration: none;
 }
 #bridal-studios
 {
-    background-image: url('https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group1095%402x.png');
+    background-image: url('https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/beautiful-bouquet-bride.jpg');
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    cursor: pointer;
+    text-decoration: none;
 }
 #venues
 {
-    background-image: url('https://d1qc9wtuffqlue.cloudfront.net/images/home-page/Group1096%402x.png');
+    background-image: url('https://hitcheed-laravel.s3-ap-southeast-1.amazonaws.com/images/banquet-beautiful-catering.jpg');
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    cursor: pointer;
+    text-decoration: none;
 }
 #photographers
 {
@@ -114,7 +138,7 @@ h1
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    cursor: pointer;
+    text-decoration: none;
 }
 #other-countries
 {
@@ -122,7 +146,7 @@ h1
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    cursor: pointer;
+    text-decoration: none;
 }
 .row1
 {
@@ -139,7 +163,6 @@ h1
     font-family: 'Cormorant Garamond';
     font-style: italic;
     font-size: 18px;
-
     cursor: pointer;
     /* text-decoration: underline; */
 }
@@ -155,5 +178,20 @@ h1
 {
     width: 100%;
     height: 255px;
+}
+
+.icon-block
+{
+    position: relative;
+    top: calc(50% - 31px);
+}
+
+.icon-text
+{
+    font-size: 14px;
+    color:white;
+    font-family: 'Open Sans';
+    font-weight: 600;
+    text-transform: uppercase;
 }
 </style>
